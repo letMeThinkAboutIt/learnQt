e631146de038a3564201fd6a37fbb68b0322b696
cclr <420338169@qq.com>
2023-04-08 10:52:33 +0800

    RGBA HSVA

--- qss/MainWindow.cpp
+++ qss/MainWindow.cpp
@@ -16,33 +16,22 @@ MainWindow::MainWindow(QWidget *parent)
 
 //testNsQss--nameSpaceBtn
 /*
-padding:50px 50px 50px 50px;
-margin:50px 50px 50px 50px;
-background-clip:margin;
-color:red;
-background-color: pink;
+hsv 色度 饱和度 亮度
+色度 0 红色； 120 绿色； 240 蓝色； 范围 0-360
+    可以理解成一个圆环，3个点将圆环均分成3分，点与点之间的颜色用插值算出来。
+饱和度 颜色与白色的混合，色度为0就是白色
+    还是圆环，圆心为色度0，圆周为色度255，一条半径上颜色越来越偏白
+    饱和度接近0，颜色几乎是灰
+亮度 亮度为0，任何颜色都为黑色
+
 */
     QString strQss = R"(
 
-        QPushButton{
-            border-style:outset;
-            border-width:10px;
-            border-color:red;
-            padding:50px 50px 50px 50px;
-            margin:50px 50px 50px 50px;
-            background-clip:margin;
-            color:red;
-            background-color: pink;
+        QMainWindow{
+            background-color:pink;
         }
-        QPushButton#localbtn{
-            border-style:outset;
-            border-width:10px;
-            border-color:red;
-            padding:-50px 50px -50px 50px;
-            margin:50px 50px 50px 50px;
-            background-clip:margin;
-            color:red;
-            background-color: pink;
+        QPushButton{
+            background-color: hsva(240, 15, 255, 255);
         }
     )";
 
